46eb3b2a6bebbb0bb94782230e412cb9e7215432
clementora <orangoclement50@gmail.com>
2026-06-23 11:40:25 +0200

    Added IMU reading on Physical car

--- control_localisation_pkg/launch/launch_fusion.py
+++ control_localisation_pkg/launch/launch_fusion.py
@@ -1,5 +1,8 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
@@ -12,8 +15,21 @@ def generate_launch_description():
         'ekf.yaml'
     )
 
+
+    use_imu_arg = DeclareLaunchArgument(
+        'use_imu',
+        default_value='true',
+        description='Set to "true" to fuse IMU via EKF, or "false" for pure wheel odometry.'
+    )
+    
+
+    use_imu = LaunchConfiguration('use_imu')
+
     return LaunchDescription([
-        #  vehicle controller node
+       
+        use_imu_arg,
+
+        # vehicle controller node (Runs no matter what)
         Node(
             package=pkg_name,
             executable='control_node',
@@ -22,21 +38,37 @@ def generate_launch_description():
             parameters=[{'use_sim_time': False}]
         ),
 
-        #  custom localiser node
+        # custom localiser node (WHEN IMU IS TRUE)
+        # Publishes to /localisation so the EKF can use it.
         Node(
             package=pkg_name,
             executable='localiser_node', 
             name='localiser',
             output='screen',
-            parameters=[{'use_sim_time': False}]
+            parameters=[{'use_sim_time': False}],
+            condition=IfCondition(use_imu)
         ),
 
-        #  robot_localization EKF node
+        # custom localiser node (WHEN IMU IS FALSE)
+        # Bypasses the EKF by renaming its output directly to /odometry/filtered.
+        Node(
+            package=pkg_name,
+            executable='localiser_node', 
+            name='localiser',
+            output='screen',
+            parameters=[{'use_sim_time': False}],
+            remappings=[('/localisation', '/odometry/filtered')],
+            condition=UnlessCondition(use_imu)
+        ),
+
+        # robot_localization EKF node
+        # Only launches if IMU is true
         Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node',
             output='screen',
-            parameters=[config_file_path, {'use_sim_time': False}]
+            parameters=[config_file_path, {'use_sim_time': False}],
+            condition=IfCondition(use_imu)
         )
     ])
